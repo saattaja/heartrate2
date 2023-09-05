f49ba4254849afa94abd6e5286665b16944ebc2d
saattaja <janita.elisa@outlook.com>
2023-09-05 17:35:05 +0300

    yees

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import React, {useState} from 'react';
 
 export default function App() {
+  const [age, setAge] = useState('');
+  const [hrLow, setHrLow] = useState (0);
+  const [hrHigh, setHrHigh] = useState (0);
+
+  function change(age){
+    setAge(age);
+    const lower = (220 - age)*0.65;
+    const higher = (220 - age)*0.85;
+    setHrLow(lower);
+    setHrHigh(higher);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Age</Text>
+      <TextInput onChangeText={age => change(age)}></TextInput>
+      <Text>Hr limits</Text>
+      <Text>{hrLow.toFixed(0)} - {hrHigh.toFixed(0)}</Text>
       <StatusBar style="auto" />
     </View>
   );
